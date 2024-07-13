1bcb9d23dd07ccc8e8a2895fafc0cc50c3f3e08b
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-07-13 12:23:55 +0600

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "REACT Test Application",
+  description:
+    "This application is developed for the interview purpose at microdeft",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
